e8cb2627ffc2e9c41581b494393ac19adae15e58
Duncan <dawitter.dw@gmail.com>
2021-12-25 18:21:24 -0700

    added Task Loop

--- Discord_web_scraper/veiwer.py
+++ Discord_web_scraper/veiwer.py
@@ -1,9 +1,6 @@
-from enum import auto
-from re import A
-from aiohttp.tracing import TraceRequestChunkSentParams
 import discord
-from discord import message
-from discord import channel 
+from discord import message, channel
+from discord.ext import tasks
 from KijijiScraperCog import KijijiScraper
 import time as time
 
@@ -38,26 +35,12 @@ async def on_message(message):
         await DeleteAllMsg(message.channel)
     
     if message.content.startswith('!start'):
-        
-        await AutoUpdate(updates)
+        await AutoUpdate.start(message.channel)
+        print('Sraper Started')
 
-@taskloop(0):
+@tasks.loop(hours=12)
 async def AutoUpdate(channel):
-    run = True
-    start = True
-    while run:
-        if start:
-            print('Web Scraper Running')
-            start = False
+    await DeleteAllMsg(channel)
+    await Update(channel)
 
-        if time.tm_hour == 12 and time.tm_min == 30:
-            await DeleteAllMsg(channel)
-            await Update(channel)
-
-
-        if time.tm_hour == 15 and time.tm_min == 30:
-            await DeleteAllMsg(channel)
-            await Update(channel)
-        
-    
 client.run(token)
